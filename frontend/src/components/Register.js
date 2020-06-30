058dd519d943b039038ff062a7cfc3eed3407834
gje9434 <optimax0@hotmail.com>
2020-07-01 00:39:19 +0100

    add register page

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
 
 function Register() {
-    return <h1>Register</h1>
+
+
+
+    return(
+    <div className="container centered">
+        <form className="standard-user-form">
+            <h2>Register</h2>
+            <input className="standard-input" type="text" placeholder="Username"/>
+            <input className="standard-input" type="password" placeholder="Password"/>
+            <input className="standard-input" type="confirm_password" placeholder="Confirm password"/>
+            <input className="standard-input" type="email" placeholder="Email address"/>
+            <button className="standard-button">Log In</button>
+            <a href="/recover">Lost your password?</a>
+        </form>
+        <span className="register-text">Already registered?
+            <a href="/login" className="register-link"> Login here!</a>
+        </span>
+    </div>
+    )
 }
 
 export default Register;
