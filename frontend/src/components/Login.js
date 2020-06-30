0cdea8d76e38bc95ae5fc39e47e46a95e5438df2
gje9434 <optimax0@hotmail.com>
2020-07-01 00:41:46 +0100

    add link to recover page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,16 +2,16 @@ import React, { useState, Fragment } from "react";
 
 function Login() {
     return (
-        <div className="container centered">
+        <div className="container">
             <form className="standard-user-form">
                 <h2>Login</h2>
                 <input className="standard-input" type="text" placeholder="Username"/>
                 <input className="standard-input" type="password" placeholder="Password"/>
                 <button className="standard-button">Log In</button>
-                <a href="">Lost your password?</a>
+                <a href="/recover">Lost your password?</a>
             </form>
             <span className="register-text">Don't have an account?
-                <a href="" className="register-link"> Sign up here!</a>
+                <a href="/register" className="register-link"> Sign up here!</a>
             </span>
         </div>
     )
