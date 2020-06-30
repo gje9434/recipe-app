f25c70dae1db39df5be8d7d955328d12911a5eb7
gje9434 <optimax0@hotmail.com>
2020-07-01 00:41:23 +0100

    remove 'about' nav link. Nav title is now a link to main recipes page

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -3,9 +3,8 @@ import React, { useState } from "react";
 function Nav() {
     return (
     <div className="nav">
-        <h1>Recipes.</h1>
+        <h1><a className="main-nav-link" href="/home">Recipes</a></h1>
         <ul className="navbar-list">
-            <li><a href="/about">About</a></li>
             <li><a href="/add">Add</a></li>
             <li><a href="">Logout</a></li>
         </ul>
